19cdd20596e362aca8c876c8026972c3392a435c
Devin <18190816904@163.com>
2022-05-23 18:22:32 +0800

    add controller and model

---
+++ blog-1/src/controller/blog.js
@@ -0,0 +1,35 @@
+const getList = (author, keyword) => {
+    // 先返回假数据(格式是正确的)
+    return [
+        {
+            id: 1,
+            title: '标题A',
+            content: '内容A',
+            createTime: 1653298124978,
+            author: 'zhangsan'
+        },
+        {
+            id: 1,
+            title: '标题B',
+            content: '内容B',
+            createTime: 1653298171069,
+            author: 'lisi'
+        },
+    ]
+}
+
+const getDetail = (id) => {
+    // 先返回假数据
+    return {
+        id: 1,
+        title: '标题A',
+        content: '内容A',
+        createTime: 1653298124978,
+        author: 'zhangsan'
+    }
+}
+
+module.exports = {
+    getList,
+    getDetail
+}
