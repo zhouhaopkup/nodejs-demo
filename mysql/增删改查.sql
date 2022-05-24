aeddb270a6524e7861644018a224dd526cd228c4
Devin <18190816904@163.com>
2022-05-24 12:33:11 +0800

    feat: con mysql

---
+++ mysql/增删改查.sql
@@ -0,0 +1,24 @@
+use myblog;
+
+-- show tables;
+
+-- insert into users(username, `password`, realname) values('zhangsan', '123', '张三');
+
+-- select * from users;
+-- select id, username from users;
+-- select * from users where username='zhangsan';
+-- select * from users where username='zhangsan' and `password`='123'
+-- select * from users where username='zhangsan' or `password`='123'
+-- select * from users where username like '%zhang%';
+-- select * from users where password like '%1%' order by id;
+-- select * from users where password like '%1%' order by id desc;
+
+-- SET SQL_SAFE_UPDATES = 0;
+-- update users set realname='李四' where username='lisi';
+
+-- delete from users where username='lisixxxxx';
+
+-- insert into blogs(title,content,createtime,author) values('标题A','内容A',1653366623760,'zhangsan');
+-- insert into blogs(title,content,createtime,author) values('标题B','内容B',1653366634808,'lisi');
+-- select * from blogs;
+
