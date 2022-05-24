d018115b537fe81df9104f00a8aeda917fce11ee
Devin <18190816904@163.com>
2022-05-24 19:13:03 +0800

    feat: login

--- blog-1/app.js
+++ blog-1/app.js
@@ -58,9 +58,16 @@ const serverHandle = (req, res) => {
         }
 
         // 处理user路由
-        const userData = handleUserRouter(req, res)
-        if(userData) {
-            res.end(JSON.stringify(userData))
+        // const userData = handleUserRouter(req, res)
+        // if(userData) {
+        //     res.end(JSON.stringify(userData))
+        //     return
+        // }
+        const userResult = handleUserRouter(req, res)
+        if(userResult) {
+            userResult.then(userData => {
+                res.end(JSON.stringify(userData))
+            })
             return
         }
 
