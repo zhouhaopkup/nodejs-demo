0384c33e5ec814306363b994a04b3f0cbcc944b1
Devin <18190816904@163.com>
2022-05-25 15:25:31 +0800

    cookies

--- blog-1/app.js
+++ blog-1/app.js
@@ -39,6 +39,20 @@ const serverHandle = (req, res) => {
     // 解析query
     req.query = querystring.parse(url.split('?')[1])
 
+    //解析cookie
+    req.cookie = {}
+    const cookieStr = req.headers.cookie || '' // k1=v1;k2=v2;k3=v3
+    cookieStr.split(';').forEach(item => {
+        if(!item) {
+            return
+        }
+        const arr = item.split('=')
+        const key = (arr[0] || '').trim()
+        const val = (arr[1] || '').trim()
+        req.cookie[key] = val
+    })
+
+
     // 处理 post data
     getPostData(req).then(postData => {
         req.body = postData
