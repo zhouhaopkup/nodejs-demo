32fad4367506c58d74e718f5094c2bdce9163032
Devin <18190816904@163.com>
2022-05-28 17:51:05 +0800

    log

--- blog-1/app.js
+++ blog-1/app.js
@@ -2,6 +2,7 @@ const querystring = require("querystring")
 const handleBlogRouter = require("./src/router/blog")
 const handleUserRouter = require("./src/router/user")
 const { getRedis, setRedis } = require("./src/db/redis")
+const { access } = require('./src/utils/log')
 
 // 获取cookie的过期时间
 const getCookieExpires = () => {
@@ -40,6 +41,9 @@ const getPostData = (req) => {
 }
 
 const serverHandle = (req, res) => {
+    // 记录access log
+    access(`${req.method} -- ${req.url} -- ${req.headers['user-agent']} -- ${Date.now()}`)
+
     // 设置返回格式 JSON
     res.setHeader('Content-type', 'application/json')
 
